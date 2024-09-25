05f7d21ced6cfe8a8d1d027a880ec115f1979054
AlmondMan <artusmosquet@gmail.com>
2024-09-24 21:10:44 -0400

    Cleaned up Rtest.py and updated the README.md file. To-do: fix deleting the temporaru files after the update is done.

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
 # python exe updater
- Updater for python executables, could also maybe be used to update non-python executables
+ Updater for python executables, can also be used to update non-python executables, like scripts
 
 
  RESTupdate.py contains a current, mostly, working build of the updater.
  
  Rtest.py is an example of how to implement RESTupdate in an updater.
- 
- NOTE: You would not have all of the code in Rtest be in your main program, it would be wiser to call the update check within your program
- and run the actual updating functions as its own independent application initially launched by your main program.
 
-Clearer explaination of how this should be used:
- 
- Check for updates in main program.
- 
- Main program launches a separate application, if it is out of date, then kills itself
- 
- This separate application then downloads the new version, cleans up and launches the new version, then kills itself (updater kills itself).
+ To implement this inside of your script or executable, you can can call the updater directly from your main script or executable, it does not have to be a separate application.
+ For example, you could place the entirety of the code in "Rtest.py" at the start of your main function if you wanted to automatically check for updates on launch.
+
+ Alternatively you can run it as a separate process but you would have to modify it to be compatible for that scenario.
--- Rtest.py
+++ Rtest.py
@@ -1,6 +1,5 @@
 import RESTupdate as rup
-import subprocess
-import sys
+
 #example of how to use the RESTupdate module to update a program
 #This will currently only update one file or executable due to how the "cleanup" function is setup
 
@@ -9,43 +8,22 @@ owner = 'amosquet' #rplace with the actual owner of the repository
 repo = 'Random-Cypher' #replace with the actual repository name
 current_version = 'v1.0.0' #replace with the current version of your program
 file = 'RCCG.py' #replace with the actual name of the file with extension, as shown, to be updated
-repow = str(owner+'/'+repo) #replace with the actual repository name
+repow = str(owner+'/'+repo) #combine the owner and repository name to form the repository path
 update_folder = "amcupdater" #name for the temporary directory to store the updated program
 
-dir = rup.directory() #get the current directory of the program, used for 
+dir = rup.directory() #get the current directory of the program, used for moving the updated program to the original directory
 # print(dir) #print the current directory of the program - for debugging purposes
 
 #check if there are updates available for the program
 if rup.check(current_version, repow): #True means out-of-date
-    print("Updates available") #maybe temporary
+
+    print("Updates available") #maybe temporary, for debugging purposes, but can be used to notify the user of updates
     rup.create_dir(update_folder)
     rup.clone(repow, update_folder)
     rup.cleanup(file, update_folder)
-    print("Update successful") #maybe temporary
+    print("Update successful") #maybe temporary, for debugging purposes, but can be used to notify the user of a successful update
     rup.py_restart(file) #for restarting python scripts
     # rup.exe_restart(file) #for restarting executables
 
 else:
-    print("No updates")
-
-
-
-
-
-
-
-#OLD CODE FROM WHEN I FIRST STARTED
-
-# # Example usage
-# owner = 'owner'  # Replace with the actual owner of the repository
-# repo = 'repo'  # Replace with the actual repository name
-# current_version = 'v1.0.0'  # Replace with the current version of your program
-# file = 'program.py'  # Replace with the actual name of the file to be updated
-# repow = 'owner/repo'  # Replace with the actual repository name
-# # repo_url = f"https://github.com/{repow}.git"
-# dir = directory()
-
-# if check(current_version, repo):
-#     update(True, repow, dir)
-# else:
-#     update(False, repow, dir)
+    print("No updates") #example of what to do if there are no updates available
